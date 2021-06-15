4e5993942d5f2ce85e2f3b6574f12a20bff688e4
natalia <nataliarrod@gmail.com>
2021-06-14 21:02:16 -0500

    project initial configuration and created first components

---
+++ src/components/Form.js
@@ -0,0 +1,61 @@
+import React, { useContext, useState } from "react";
+import { CategoriesContext } from "../context/ContextCategories";
+
+const Form = () => {
+  const {categories} = useContext(CategoriesContext);
+  const [search, setSearch] = useState({
+    name: '',
+    category: '',
+  });
+  const getDataRecipe = e => {
+    setSearch({
+      ...search,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  return (
+    <form className="col-12">
+      <fieldset className="text-center">
+        <legend>Search drinks for categories or ingredients</legend>
+      </fieldset>
+
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            name="name"
+            className="form-control"
+            type="text"
+            placeholder="Search for an ingredient"
+            onChange={getDataRecipe}
+          />
+        </div>
+        <div>
+          <select 
+            className="form-control" 
+            name="category"
+            onChange={getDataRecipe}
+          >
+            <option value="">--Select a category--</option>
+            {categories.map(category => (
+              <option 
+                key={category.strCategory}
+                value={category.strCategory}
+              >
+              {category.strCategory}</option>
+            ))}
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Search for drinks"
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
